ed17f85a0bc791ddebbf6688eafc12c326c5f9be
70NYDR01DD <tonydroidd@gmail.com>
2015-04-25 19:23:51 -0600

    Update Main.php

--- src/TDroidd/SignTransfer/Main.php
+++ src/TDroidd/SignTransfer/Main.php
@@ -8,35 +8,22 @@ use pocketmine\plugin\PluginManager;
 use shoghicp\FastTransfer;
 use pocketmine\tile\Sign;
 use pocketmine\event\player\PlayerInteractEvent;
-class Main extends PluginBase implements Listener {
 
+class Main extends PluginBase implements Listener {
     public function onEnable(){
      if ($this->getServer()->getPluginManager()->getPlugin("FastTransfer")){
      $this->getLogger()->info("Enabling Add-On for FastTransfer");
+	 $this->getServer()->getPluginManager()->registerEvents($this, $this);
      }
 }
 	private function checkSign(Player $pl,array $sign) {
-     	if (isset($this->text["Transfer"][$sign[0]])) {
+     	if (isset($this->text[Transfer][$sign[0]])) {
 			// Fast transfer!
 			if(!$pl->hasPermission("sign.transfer.place")) {
 				$this->breakSign($pl,$tile,"You do not have permissions\nTto make a Transfer sign.");
 				return;
-				´}
+				}
      	}
-     	
-     		public function playerTouchIt(PlayerInteractEvent $event){
-		if($event->getBlock()->getId() != Block::SIGN_POST &&
-			$event->getBlock()->getId() != Block::WALL_SIGN) return;
-		$pl = $event->getPlayer();
-		$sign = $pl->getLevel()->getTile($event->getBlock());
-		if(!($sign instanceof Sign)) return;
-		$sign = $sign->getText();
-		if (!isset($this->text["sign"][$sign[0]])) return;
-
-		if(!$pl->hasPermission("sign.transfer.touch")) {
-			$pl->sendMessage("Nothing happens...");
-			return;
-		}
 		if ($event->getItem()->getId() == Item::SIGN) {
 			// Check if the user is holding a sign this stops teleports
 			$pl->sendMessage("Can not teleport while holding sign!");
@@ -46,6 +33,14 @@ class Main extends PluginBase implements Listener {
 			$pl->sendMessage("Did you expect something to happen?");
 			return;
 		}
+		if (isset($this->text[Transfer][$sign[0]])) {
+			$address = $sign[1];
+			$port = $sign[2];
+			if (empty($address)) return null;
+			$port = intval($port);
+			if ($port == 0) $port = 19132; // Default for Minecraft PE
+			return [$address,$port];
+		}
 		$this->teleporters[$pl->getName()] = time();
 		$ft = $this->getServer()->getPluginManager()->getPlugin("FastTransfer");
 		if (!$ft) {
@@ -54,7 +49,6 @@ class Main extends PluginBase implements Listener {
 			$pl->sendMessage("Someone has removed FastTransfer.");
 			return;
 		}
-		list($addr,$port) = $pos;
 		$this->getLogger()->info("- Player:  ".$pl->getName()." => ".
 										 $addr.":".$port);
 		$ft->transferPlayer($pl,$addr,$port);
@@ -62,4 +56,3 @@ class Main extends PluginBase implements Listener {
 			$this->getServer()->broadcastMessage($pl->getName()." transferred...");
 		}
 	}
-}
